74f23b6746328043a8f58051223e0ad07c508147
Van-molinari <vanessabmolinari@hotmail.com>
2023-10-25 00:30:23 -0300

    Changing to Python

---
+++ src/globals.py
@@ -0,0 +1,12 @@
+STUDENTS = [
+    # A fazer: definir os usuários que serão estudantes
+    "Van-molinari",
+    "lucasc-costa",
+    "PachecoVitor02",
+    "PauloCaramigo",
+    "walisxn",
+]
+
+TEACHERS = [
+    "mrebello",
+]
